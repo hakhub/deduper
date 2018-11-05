a617c3933102f558f2f5255eac9c0598607ba283
Rogue <rogue@froodi.com>
2018-11-05 22:41:29 +0100

    Adds printing @moduledoc, cleans up printed messages and improves process duration info (el;apsed time).

--- lib/deduper.ex
+++ lib/deduper.ex
@@ -5,6 +5,8 @@ defmodule Deduper do # Start of module
   This module identifies duplicate files within a directory tree, shows them,
   and (when it's programmed ;-) will delete selected duplicates, when told to do so.
   Syntax:           Deduper.find_dups( path, option, [extension(s)] ), 
+                    where path is the root of the directory-tree to be traversed
+                    (absolute path: don't forget the slash at the beginning.)
                     where option = audio, document, image, video, all or other (other uses the extensions).
   Author:           Rogue
   Lango:            Elixir
@@ -55,10 +57,12 @@ defmodule Deduper do # Start of module
       end
 
     # This part provides process config information...
-    IO.puts "==== COMMAND INFO ===="
+    IO.puts "==== COMMAND INFO ========"
+    IO.puts "#{@moduledoc}"
+
+    IO.puts "==== USER CONFIG INFO ===="
     IO.puts "Path (the root of the tree): #{path} ."
-    IO.puts "(Absolute path: don't forget the slash at the beginning.)"
-    IO.puts "Extensions to look for (option #{option}): #{ftypes} )."
+    IO.puts "Extensions to look for (option: #{option}): #{ftypes} ."
     IO.puts "Please take a cup of tea..."
 
     # This line creates the wildcard that defines what files to look for...
@@ -68,7 +72,7 @@ defmodule Deduper do # Start of module
     start_time = System.monotonic_time(:millisecond)                # Start the VM clock/timer
 
     # MAIN ###
-    IO.puts "==== PROGRESS INFO ===="
+    IO.puts "==== PROGRESS INFO ======="
     IO.puts "... change directory to path..."
     File.cd!(path, fn ->
       # TODO: Path.wildcard("#{path}/**/*.{#{ftypes}}")
@@ -78,8 +82,8 @@ defmodule Deduper do # Start of module
       Path.wildcard("#{wildcard}")                                  # Show all relevant files in tree
       #IO.puts "... calculating hash per file, and clustering..."
       |> Enum.filter( fn(filename) -> File.regular?(filename) end)  # Weed out dead links, etc.
-      |> Enum.group_by( fn(filename) ->                             # Calc sha256 per file, and group 'm.
-           # MD5 takes #{ (4088/7606) * 100}%, approx 54%, of the time SHA256 takes on 548 files."
+      |> Enum.group_by( fn(filename) ->                             # Calc hash per file, and group 'm.
+           # Performance info: MD5 takes approx 54%, of the time SHA256 takes on the same 548 files, but SHA256 is more thourough.
            "#{ :crypto.hash( :md5, File.read!("#{filename}") ) |> Base.encode16 }"
            # "#{ :crypto.hash( :sha256, File.read!("#{filename}") ) |> Base.encode16 }"
           end)
@@ -91,13 +95,14 @@ defmodule Deduper do # Start of module
           # DO SOMETHING WITH THE DUPLICATES FOUND...
           nr_of_dups = Enum.count(files)
           IO.puts "--- Start of #{ nr_of_dups } duplicate files ---"
-          # TODO: While length(files) > 1 AND user_input <> do_nothing do...
-          #   ask which file may be deleted, rinse and repeat
           duplicates = Enum.with_index(files, 1)                  # Add an index-nr to each duplicate
           Enum.each(duplicates, fn { filename, index} ->
               IO.puts"#{index} - #{filename}"
             end)
           # TODO: Select files for deletion, renaming, printing, encrypting, reverse engineering or whatnot...
+          # Pseudo code:
+          # WHILE length(files) > 1 AND user_input <> do_nothing DO...
+          #   ask which file may be deleted, rinse and repeat
           IO.puts "--- End of duplicate files  ---"
 
         end) # end of Enum.each
@@ -108,8 +113,11 @@ defmodule Deduper do # Start of module
     # used_time = stop_time - start_time in milliseconds
     stop_time = System.monotonic_time(:millisecond)                 # Read process end-time of VM clock/timer
     diff = stop_time - start_time                                   # Process-time is difference between start- and end-time
-    IO.puts "==== END INFO ===="
-    IO.puts "Processing took #{ diff/1000 } seconds, roughly #{ Float.floor( diff/60_000, 2) } minutes."
+    elapsed_seconds = diff/1000
+    elapsed_minutes = Float.floor( diff/60_000, 2)
+    elapsed_hours   = Float.floor( diff/3_600_000, 2)
+    IO.puts "==== END INFO ============"
+    IO.puts "Processing took #{ elapsed_seconds } seconds, roughly #{ elapsed_minutes } minutes or #{ elapsed_hours } hours."
     IO.puts ".oOo."
 
   end # End of find_dups function
